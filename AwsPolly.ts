7c45f73b58d3d63c5fc6f13bf31ebe2769c3669e
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-25 10:28:43 +0200

    aws polly added

---
+++ AwsPolly.ts
@@ -0,0 +1,49 @@
+import * as dotenv from "dotenv";
+import WebSocketManager from "./WebSocketManager";
+import { AwsCredentialIdentity } from "@aws-sdk/types";
+import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+
+dotenv.config();
+
+const callback = async (connectionId: string, message: string) => {
+  console.log("Message received:", JSON.parse(message));
+
+  const input = JSON.parse(message);
+
+  const awsCredentials: AwsCredentialIdentity = {
+    accessKeyId: process.env.AWS_ACCESS_KEY || "",
+    secretAccessKey: process.env.AWS_SECRET_KEY || "",
+  };
+
+  const polly = new PollyClient({
+    region: process.env.AWS_REGION || "",
+    credentials: awsCredentials,
+  });
+  const params = {
+    OutputFormat: "mp3",
+    Engine: "neural",
+    LanguageCode: "es-ES",
+    Text: `<speak>${input.text}</speak>`,
+    VoiceId: "Lucia",
+    TextType: "ssml",
+  };
+  polly.send(new SynthesizeSpeechCommand(params), async (error, data) => {
+    if (error) {
+      webSocketManager.sendMessage(
+        connectionId,
+        JSON.stringify({
+          data: null,
+        })
+      );
+    } else if (data) {
+      webSocketManager.sendMessage(
+        connectionId,
+        JSON.stringify({
+          data: await data.AudioStream.transformToByteArray(),
+        })
+      );
+    }
+  });
+};
+
+const webSocketManager = new WebSocketManager(callback);
--- client.ts
+++ client.ts
@@ -7,10 +7,14 @@ const ws = new WebSocket("ws://127.0.0.1:8080/", {
 ws.on("error", console.error);
 
 ws.on("open", function open() {
-  const data = [{ role: "user", content: "Cual es la capital de Cuba?" }];
-  ws.send(JSON.stringify(data));
+  const data = "Hola mi nombre es Lucia";
+  ws.send(
+    JSON.stringify({
+      text: data,
+    })
+  );
 });
 
 ws.on("message", function message(data) {
-  console.log("received: %s", data.slice(0));
+  console.log("received: %s", JSON.parse(data.toString())["data"]);
 });
