ccd2fb29592b55b6823437f095e752bdce8a6813
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-25 08:01:31 +0200

    open and close connection managing

--- server.ts
+++ server.ts
@@ -4,27 +4,41 @@ import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
-const server = new WebSocket.Server({ port: Number(process.env.PORT!) });
+const server = new WebSocket.Server({ port: 8080 });
 
-const clients: { [key: string]: WebSocket } = {};
+const connections: { [key: string]: WebSocket } = {};
+
+const storeConnection = (uuid: string, connection: WebSocket) => {
+  console.log(`Received a new connection (ID: ` + uuid + `)`);
+  connections[uuid] = connection;
+  console.log(`Total connections open: ` + Object.keys(connections).length);
+};
+
+const deleteConnection = (uuid: string) => {
+  console.log(`Connection closed (ID: ` + uuid + `)`);
+  delete connections[uuid];
+  console.log(`Total connections open: ` + Object.keys(connections).length);
+};
 
 server.on("connection", (connection) => {
   const userId = uuidv4();
 
-  console.log(`Received a new connection: ` + userId);
+  storeConnection(userId, connection);
 
-  clients[userId] = connection;
-
-  connection.on("message", (message) => {
-    console.log("Message received:", message);
-
-    connection.binaryType = "arraybuffer";
-
-    const arrayBuffer = new ArrayBuffer(123); // Tu ArrayBuffer de ejemplo
-    const blob = new Blob([arrayBuffer]);
-
-    connection.send(arrayBuffer);
+  connection.on("close", () => {
+    deleteConnection(userId);
   });
+
+  // connection.on("message", (message) => {
+  //   console.log("Message received:", message);
+
+  //   connection.binaryType = "arraybuffer";
+
+  //   const arrayBuffer = new ArrayBuffer(123); // Tu ArrayBuffer de ejemplo
+  //   const blob = new Blob([arrayBuffer]);
+
+  //   connection.send(arrayBuffer);
+  // });
 });
 
 console.log("Server running on port 8080");
